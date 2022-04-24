8c4fa8a594cfdcb01cc1efdd39c3d1478ff5ff6a
deepakrajpal27 <deepakrajpal27@gmail.com>
2022-04-24 03:41:54 +0200

    Removed unwanted import

--- src/AboutUs/index.js
+++ src/AboutUs/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Typography } from '@mui/material';
 import useStyles from './styles';
-import deepakImage from "./../images/deepak.png"
 
 const AboutUs = () => {
 
