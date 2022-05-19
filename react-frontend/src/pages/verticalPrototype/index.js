43e799054398bc423f2aac27d65687c166d450c4
Sagar10111996 <sagardhaware25@gmail.com>
2022-05-19 13:37:12 +0200

    Upload modal button added to nav bar

--- react-frontend/src/pages/verticalPrototype/index.js
+++ react-frontend/src/pages/verticalPrototype/index.js
@@ -4,8 +4,13 @@ import { setPageName } from '../../redux/slice/pagename';
 import useStyles from './styles';
 import { getMedia } from './../../services/user';
 import { Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+import UploadModal from '../../components/uploadModal'
+import { setUploadModal } from '../../redux/slice/uploadModal'
 
 const VerticalPrototype = () => {
+
+    const displayUploadModal = useSelector((state) => state.uploadModal.displayUploadModal)
 
     const dispatch = useDispatch();
     const [media, setMedia] = useState([]);
@@ -14,17 +19,31 @@ const VerticalPrototype = () => {
         dispatch(setPageName("Home"))
     })
 
+    async function fetchMedia() {
+        const response = await getMedia()
+        setMedia(response);
+    }
+
     useEffect(() => {
-        async function fetchMedia() {
-            const response = await getMedia()
-            setMedia(response);
-        }
         fetchMedia();
     }, [])
 
+    const closeModal = () => {
+      dispatch(setUploadModal(false))
+      fetchMedia();
+
+    }
+    console.log({displayUploadModal})
     const classes = useStyles();
     return (
         <div className={classes.root}>
+            {
+                displayUploadModal
+                ?
+                    <UploadModal closeModal={closeModal} />
+                : 
+                    null
+            }
             {(media && media.length !== 0) && media.map((mediaItem, index) => {
                 return <div key={index} className={classes.imageCard} >
                     <img alt={mediaItem.name} className={classes.imageProps} src={mediaItem.url} />
