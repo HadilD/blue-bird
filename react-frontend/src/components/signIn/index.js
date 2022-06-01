f44529929d456eb266d40dbc84aa2784a66e5b05
Sagar10111996 <sagardhaware25@gmail.com>
2022-06-01 12:37:15 +0200

    signIn page css

--- react-frontend/src/components/signIn/index.js
+++ react-frontend/src/components/signIn/index.js
@@ -1,72 +1,82 @@
 import React from 'react'
 import { Formik } from 'formik';
 import { loginUser } from '../../services/auth'
+import useStyles from './styles';
 
 function SignIn (props) {
-    const {setSignInDisplay} = {...props}
-    return (
-    <div>
-      <Formik
-        initialValues={{ email: '', password: '' }}
-        validate={values => {
-          const errors = {};
-          if (!values.email) {
-            errors.email = 'Required';
-          } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-              errors.email = 'Invalid email address';
-          } else if (values.email.slice(values.email.length-12) !== '.hs-fulda.de'){
-              errors.email = 'Not a member of Fulda organization'
-          }
+  const classes = useStyles();
+  const {setSignInDisplay} = {...props}
 
-          if (!values.password) {
-            errors.password = 'Required'; 
-          }
-          return errors;
-        }}
-        
-        onSubmit={(values, actions) => {
-          console.log('submit clicked:', values)
-          loginUser(values)
-          actions.resetForm({
-            values: {email: '', password: ''}
-          });
+  return (
+  <div className={classes.signInContainer}>
+    <h2 className={classes.heading}>Sign In</h2>
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validate={values => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+            errors.email = 'Invalid email address';
+        } else if (values.email.slice(values.email.length-12) !== '.hs-fulda.de'){
+            errors.email = 'Not a member of Fulda organization'
+        }
 
-        }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          resetForm,
-          handleChange,
-          handleSubmit,
-        }) => (
-          <form onSubmit={handleSubmit} style={{display: 'flex', flexDirection: 'column'}}>
-            <label>Email</label>
+        if (!values.password) {
+          errors.password = 'Required'; 
+        }
+        return errors;
+      }}
+      
+      onSubmit={(values, actions) => {
+        console.log('submit clicked:', values)
+        loginUser(values)
+        actions.resetForm({
+          values: {email: '', password: ''}
+        });
+
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        resetForm,
+        handleChange,
+        handleSubmit,
+      }) => (
+        <form onSubmit={handleSubmit} className={classes.formContainer}>
+          <div className={classes.formControl}>
+            <label className={classes.label}>Email</label>
             <input
+              className={classes.input}
               type="email"
               name="email"
               onChange={handleChange}
               value={values.email}
             />
-            {errors.email && touched.email && errors.email}
-            <label>Password</label>
+            <p className={classes.errorMsg}>{errors.email && touched.email && errors.email}</p>
+          </div>
+          <div className={classes.formControl}>
+            <label className={classes.label}>Password</label>
             <input
+              className={classes.input}
               type="password"
               name="password"
               onChange={handleChange}
               value={values.password}
             />
-            {errors.password && touched.password && errors.password}
-            <button type="submit" disabled={false}>
-              Submit
-            </button>
-          </form>
-        )}
-      </Formik>
-      <p>Don't have an account ? <span onClick={() => setSignInDisplay(false)}>Register with Us</span></p>
+            <p className={classes.errorMsg}>{errors.password && touched.password && errors.password}</p>
+          </div>
+          <button type="submit" disabled={false} className={classes.button}>
+            Submit
+          </button>
+        </form>
+      )}
+    </Formik>
+    <p className={classes.signUpStatement}>Don't have an account ? <span className={classes.register} onClick={() => setSignInDisplay(false)}>Register with Us</span></p>
   </div>
-    )
+  )
 }
 
 export default SignIn
--- react-frontend/src/pages/Login/styles.js
+++ react-frontend/src/pages/Login/styles.js
@@ -1,14 +1,13 @@
 import { makeStyles } from '@mui/styles';
-// import { generalStyles } from '../../generalStyles';
 
 const useStyles = makeStyles(theme => ({
     container: {
         display: 'flex',
         flexDirection: 'column',
         margin: 0,
-        minHeight: '80vh',
         justifyContent: 'center',
         alignItems: 'center',
+        minHeight: '80vh',
     },
 }))
 
