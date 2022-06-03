c4e8d97238894d240d6a0ec132464031eec91d48
segullshairbutt <segullshairbutt@gmail.com>
2022-06-03 17:44:19 +0200

    keep my backend

--- django_backend/chat/models.py
+++ django_backend/chat/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Message(models.Model):
     username = models.CharField(max_length=255)
     room = models.CharField(max_length=255)
@@ -10,4 +11,5 @@ class Message(models.Model):
         ordering = ('date_added',)
 
     def __str__(self):
-        return 'Room name: %s , Username: %s , Message: %s , Time: %s' % (self.room, self.username, self.content, self.date_added)
+        return 'Room name: %s , Username: %s , Message: %s , Time: %s' % (
+        self.room, self.username, self.content, self.date_added)
