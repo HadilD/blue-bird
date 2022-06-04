039fcabdf53221a20409f91433a2cd1181af4dcb
Sagar10111996 <sagardhaware25@gmail.com>
2022-06-04 14:38:12 +0200

    Api endpoint updated

--- react-frontend/src/config/index.js
+++ react-frontend/src/config/index.js
@@ -1,5 +1,5 @@
 // const APIEndpoint = 'https://bluebird.no-ip.org/'
 //http://127.0.0.1:8000/
-const APIEndpoint = 'http://127.0.0.1:8000/'
+const APIEndpoint = 'https://bluebird.no-ip.org/'
 
 export default APIEndpoint
