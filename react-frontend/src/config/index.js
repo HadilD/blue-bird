a4bfe18b3e771b86a854fd35922adc2df327cd9a
segullshairbutt <segullshairbutt@gmail.com>
2022-08-06 13:41:13 +0200

    frontend docker application is made ready

--- react-frontend/src/config/index.js
+++ react-frontend/src/config/index.js
@@ -1,4 +1,5 @@
-const APIEndpoint = 'https://bluebird.no-ip.org/'
+// const APIEndpoint = 'https://bluebird.no-ip.org/'
+const APIEndpoint = process.env.REACT_APP_API_URL
 
 
 export default APIEndpoint
